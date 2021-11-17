7e3f37261d5c5bd30530682cb37926250be1bdc3
danny-flahive <93837720+danny-flahive@users.noreply.github.com>
2021-11-17 14:10:10 +0000

    Updated Regex

--- Program.cs
+++ Program.cs
@@ -10,19 +10,16 @@ namespace EmailExtraction
     {
         static void Main(string[] args)
         {
-            //Regex softwireEmail = new Regex("\\s[a-zA-Z]+@softwire.com");
-            //Console.WriteLine($"There are {softwireEmail.Matches(text).Count} matches");
-
             string text = File.ReadAllText(@"C:\Users\Danny.Flahive\Training\EmailExtraction\input.txt");
-            Regex email = new Regex("[a-zA-Z0-9_\\-\\.]+@[a-zA-Z0-9\\-]+.(com|co.uk|org|edu|au|nz)");
+            Regex email = new Regex(@"\s[a-zA-Z0-9_\-\.\'\%\+]+@[a-zA-Z0-9\\-]+.(com|co.uk|org|edu|au|nz|co.nz|net)\s");
             Dictionary<string, int> domainFrequecies = new Dictionary<string, int>();
             foreach (Match match in email.Matches(text))
             {
                 string matchStr = match.Value;
-                int ampersandIndex = matchStr.IndexOf("@");
-                int topLevelDomainIndex = matchStr.Substring(ampersandIndex).IndexOf(".");
-                string domain = matchStr.Substring(ampersandIndex, topLevelDomainIndex);
-                domainFrequecies[domain] = domainFrequecies.ContainsKey(domain) ? domainFrequecies[domain] + 1 : 0;
+                int atIndex = matchStr.IndexOf("@");
+                int topLevelDomainIndex = matchStr.Substring(atIndex).IndexOf(".");
+                string domain = matchStr.Substring(atIndex, topLevelDomainIndex);
+                domainFrequecies[domain] = domainFrequecies.ContainsKey(domain) ? domainFrequecies[domain] + 1 : 1;
             }
             var sortedDomainFrequencies = domainFrequecies.OrderByDescending(p => p.Value);
             foreach (var pair in sortedDomainFrequencies)
